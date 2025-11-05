9c560334f3a229acac617c297ec9545e8c79df28
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2025-11-05 17:38:50 -0500

    file writing working -- no set file location

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,21 +3,16 @@ use std::fs::File;
 use std::io::prelude::*;
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-fn save_markdown_file(text: String) -> Result<()> {
-    let mut file = File::create("writing.md")?;
-    file.write_all(text.as_bytes())?;
+fn save_markdown_file(text: String) -> Result<(), String> {
+    let mut file = File::create("test.md").map_err(|err| err.to_string())?;
+    file.write_all(text.as_bytes()).map_err(|err| err.to_string())?;
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .invoke_handler(tauri::generate_handler![save_markdown_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
