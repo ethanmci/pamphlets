c13131b87c4ea14804721ef7f8d217b5f8a3f599
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2025-11-07 13:27:05 -0500

    starting on file directory selector back-end

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,54 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::PathBuf;
+use std::sync::Mutex;
+use tauri::{Manager, State};
+use tauri_plugin_dialog::DialogExt;
+
+#[derive(Default)]
+struct AppState {
+    active_file: PathBuf,
+}
 
 #[tauri::command]
 fn save_markdown_file(text: String) -> Result<(), String> {
     let mut file = File::create("test.md").map_err(|err| err.to_string())?;
-    file.write_all(text.as_bytes()).map_err(|err| err.to_string())?;
+    file.write_all(text.as_bytes())
+        .map_err(|err| err.to_string())?;
+    Ok(())
+}
+
+#[tauri::command]
+fn set_base_dir(
+    state: State<'_, Mutex<AppState>>,
+    dir_string_to_parse: String,
+) -> Result<(), String> {
+    let mut state = state.lock().unwrap();
+    state.active_file = PathBuf::from(dir_string_to_parse);
+    //Ok(state.active_file) => println!("Result: {}", state.active_file)
+    Ok(())
+}
+
+#[tauri::command]
+async fn get_base_dir(state: State<'_, Mutex<AppState>>) -> Result<(), String> {
+    let state = state.lock().unwrap();
+    state.active_file.display().to_string();
     Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![save_markdown_file])
+        .invoke_handler(tauri::generate_handler![save_markdown_file, set_base_dir])
+        .setup(|app| {
+            app.manage(Mutex::new(AppState {
+                active_file: PathBuf::new(),
+            }));
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
